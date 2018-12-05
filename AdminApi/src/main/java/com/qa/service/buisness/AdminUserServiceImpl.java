a6e0b22c5a7ed6ef946d190a291ca28af3d6c7c7
ahmedmardiosman <ahmedmardiosman@gmail.com>
2018-12-05 15:08:24 +0000

    Admin User Service Implementation Updated

---
+++ AdminApi/src/main/java/com/qa/service/buisness/AdminUserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.qa.service.buisness;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.persistence.domain.AdminUser;
+import com.qa.persistence.repository.AdminUserRepository;
+
+@Service
+public class AdminUserServiceImpl implements AdminUserService {
+
+	@Autowired
+	private AdminUserRepository adminUserRepo;
+
+	@Autowired
+	private AdminUser user;
+
+	public AdminUser addAdminAccount(String userName, String password) {
+		user.setUserName(userName);
+		user.setPassword(password);
+		return adminUserRepo.save(user);
+	}
+
+	public Iterable<AdminUser> getAllAdminAccounts() {
+		return adminUserRepo.findAll();
+	}
+
+	public Optional<AdminUser> getAdminAccount(Long adminId) {
+		return adminUserRepo.findById(adminId);
+	}
+
+	public String deleteAdminAccount(Long adminId) {
+		adminUserRepo.deleteById(adminId);
+		return "Admin User Successfully Deleted";
+
+	}
+
+}
