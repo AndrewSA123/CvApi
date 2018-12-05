d8f27931ba6477846066038b9fcd386d5d365ee0
ahmedmardiosman <ahmedmardiosman@gmail.com>
2018-12-05 15:07:32 +0000

    Admin User Service Updated

--- AdminApi/src/main/java/com/qa/service/buisness/AdminUserService.java
+++ AdminApi/src/main/java/com/qa/service/buisness/AdminUserService.java
@@ -5,13 +5,13 @@ import java.util.Optional;
 import com.qa.persistence.domain.AdminUser;
 
 public interface AdminUserService {
-	
-String addAdminAccount(String userName, String password);
-	
+
+	AdminUser addAdminAccount(String userName, String password);
+
 	Iterable<AdminUser> getAllAdminAccounts();
-	
+
 	Optional<AdminUser> getAdminAccount(Long adminId);
-	
+
 	String deleteAdminAccount(Long adminId);
 
 }
