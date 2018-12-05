ef699339fe33bdc3fcbeb5e6b80dad1b7fe3d50d
ahmedmardiosman <ahmedmardiosman@gmail.com>
2018-12-05 14:44:07 +0000

    Admin User Domain Created

---
+++ AdminApi/src/main/java/com/qa/persistence/domain/AdminUser.java
@@ -0,0 +1,45 @@
+package com.qa.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class AdminUser {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long adminId;
+	private String userName;
+	private String password;
+
+	public AdminUser() {
+
+	}
+
+	public Long getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(Long adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
