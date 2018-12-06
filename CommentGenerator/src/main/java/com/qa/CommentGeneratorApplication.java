bd5e9ba37c24bc2c32d55f9bd67c2ef4da78a5bf
ahmedmardiosman <ahmedmardiosman@gmail.com>
2018-12-06 12:41:59 +0000

    Comment Generator Application Updated

--- CommentGenerator/src/main/java/com/qa/CommentGeneratorApplication.java
+++ CommentGenerator/src/main/java/com/qa/CommentGeneratorApplication.java
@@ -2,6 +2,8 @@ package com.qa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CommentGeneratorApplication {
@@ -9,4 +11,10 @@ public class CommentGeneratorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CommentGeneratorApplication.class, args);
 	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
 }
