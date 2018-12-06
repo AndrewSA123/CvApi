20b58a6be954c995e92fd05ff034968dda549f4e
ahmedmardiosman <ahmedmardiosman@gmail.com>
2018-12-06 12:42:09 +0000

    Admin API updated

--- AdminApi/src/main/java/com/qa/AdminApiApplication.java
+++ AdminApi/src/main/java/com/qa/AdminApiApplication.java
@@ -1,16 +1,8 @@
 package com.qa;
 
-import javax.jms.ConnectionFactory;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
