b69f8f491b967bd6c392b936cec469ea2b68077e
andrewsa123 <andrewknott_123@hotmail.co.uk>
2018-12-10 09:44:52 +0000

    Updating

--- CommentGenerator/src/main/java/com/qa/persistence/domain/Comment.java
+++ CommentGenerator/src/main/java/com/qa/persistence/domain/Comment.java
@@ -29,6 +29,13 @@ public class Comment {
 		this.comment = comment;
 	}
 	
+	public Long getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(Long user_id) {
+		this.user_id = user_id;
+	}
 	public Long getId() {
 		return id;
 	}
